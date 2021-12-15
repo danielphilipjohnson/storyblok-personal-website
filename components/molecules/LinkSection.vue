65d1141280222ed1856bc9b5e0ebbb19e353452c
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-12-15 21:18:52 +0000

    Added molecules

---
+++ components/molecules/LinkSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="classes">
+    <Heading tag="h2" :content="title" />
+    <Link-List :links="links" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classes: {
+       type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    links: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ components/molecules/LinkSocials.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="flex items-center">
+    <li class="mb-2 mr-4" v-for="link in links" :key="link.link">
+      <!-- <External-Link
+        class="text-white"
+        link="https://dev.to/danielphilipjohnson"
+      >
+        <BaseIcon
+          icon="github"
+          :link="'/github'"
+          accessible_text="link.accessible_text"
+        />
+      </External-Link> -->
+
+      <BaseIcon
+        :icon="link.icon"
+        :link="link.link"
+        :accessible_text="link.alt"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>

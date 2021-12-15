a2420fbb482c5af784b84f2c6fde10880486f6f3
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-12-15 21:18:21 +0000

    Added base components

---
+++ components/base/button.vue
@@ -0,0 +1,73 @@
+<template>
+    <NuxtLink
+    v-if="type === 'primary'"
+      class="
+        border border-white
+        text-white
+        mr-4
+        py-2
+        px-3
+        bg-primary
+        text-sm
+        font-bold
+        hover:opacity-75
+      "
+      :to="link"
+    >
+      {{ text }}
+    </NuxtLink>
+
+    <NuxtLink
+     v-else-if="type === 'secondary'"
+      class="
+        border border-white
+        text-white
+        py-2
+        px-4
+        bg-primary-dark
+        text-sm
+        font-bold
+        hover:opacity-75
+      "
+      :to="link"
+    >
+      {{ text }}
+    </NuxtLink>
+
+
+    <NuxtLink
+    v-else
+      class="
+        border border-white
+        text-white
+        py-2
+        px-4
+        text-sm
+        font-bold
+        hover:opacity-75
+      "
+      :to="link"
+    >
+      {{ text }}
+    </NuxtLink>
+ 
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Object,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ components/base/icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="source === 'fa'">
+    <font-awesome-icon :icon="icon" />
+  </div>
+
+  <IconSocial v-else :icon="icon" :link="link" :accessible_text="accessible_text" />
+
+</template>
+
+<script>
+export default {
+  props: {
+    source: {
+      type: String,
+      default: "",
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    accessible_text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>

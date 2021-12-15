e619683b998a8f35e151b2857ac77623a443f032
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-12-15 21:18:34 +0000

    Added atoms

---
+++ components/atoms/Paragraph.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class="pb-2">
+    {{ content }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>

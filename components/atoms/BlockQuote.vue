fa8cfcc821b18e7a1f0f3f3da64d4acf6eefaeb4
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-12-21 19:07:29 +0000

    Added blockquote to atoms

---
+++ components/atoms/BlockQuote.vue
@@ -0,0 +1,23 @@
+<template>
+    <blockquote class="leading-loose mb-3 blockquote">
+        {{ text }}
+         <slot></slot>
+    </blockquote>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+<style scoped>
+.blockquote {
+    border-left: 3px solid #3a3c55;
+    padding-left: 1.25rem
+}
+</style>

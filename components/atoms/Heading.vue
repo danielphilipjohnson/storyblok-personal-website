d860af594a66ed9f1dc6b16aad5f144ad756eaec
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-12-21 19:07:09 +0000

    Updated atoms heading to apply heading style

--- components/atoms/Heading.vue
+++ components/atoms/Heading.vue
@@ -20,9 +20,9 @@ export default {
     return {
       classes: {
         h1: "text-3xl md:text-4xl font-bold pb-2 mb-3 border-b border-grey",
-        h2: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
-        h3: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
-        h4: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h2: "text-3xl md:text-4xl mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h3: "text-2xl md:text-3xl mr-8 uppercase text-sm font-bold text-light-gray mb-2",
+        h4: "text-xl md:text-2xl mr-8 uppercase text-sm font-bold text-light-gray mb-2",
         h5: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
         h6: "mr-8 uppercase text-sm font-bold text-light-gray mb-2",
       },

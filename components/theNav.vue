058a38e148e398a5b1481beb179f6fcb3b8dcf03
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-01-04 11:54:49 +0000

    Re order the nav tailwind classes

--- components/theNav.vue
+++ components/theNav.vue
@@ -1,72 +1,43 @@
 <template>
-  <nav class="nav dark:bg-black text-black dark:lg:text-white">
-    <div class="container mx-auto nav-content">
-      <ul class="nav-links">
-        <li><span role="img" aria-label="dark mode">🌕</span></li>
+  <nav
+    class="sticky top-0 z-50 px-4 py-3 text-black bg-white border-b border-grey nav dark:bg-black dark:text-white"
+  >
+    <div class="container flex items-center justify-between mx-auto">
+      <ul class="flex justify-between">
+        <li>
+          <nuxt-link class="block" to="/" aria-label="home">
+            <img class="w-12 h-12" src="~/assets/icon/icon.png" />
+          </nuxt-link>
+        </li>
       </ul>
-      <ul class="nav-links">
-        <li>
-          <nuxt-link class="nav-link" to="/">/</nuxt-link>
+      <ul class="flex justify-between nav-links">
+        <li class="px-2 py-1 text-sm transition-colors duration-500">
+          <span role="img" aria-label="dark mode">🌕</span>
         </li>
-        <li class="hidden md:block">
-          <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+
+        <li
+          class="hidden px-2 py-1 text-sm transition-colors duration-500 md:block"
+        >
+          <nuxt-link class="font-extrabold hover:opacity-60" to="/about"
+            >About</nuxt-link
+          >
         </li>
-        <li>
-          <nuxt-link class="nav-link" to="/blog">Blog</nuxt-link>
+        <li class="px-2 py-1 text-sm transition-colors duration-500">
+          <nuxt-link class="font-extrabold hover:opacity-60" to="/blog"
+            >Blog</nuxt-link
+          >
         </li>
-        <li>
-          <nuxt-link class="nav-link" to="/projects">Projects</nuxt-link>
+        <li class="px-2 py-1 text-sm transition-colors duration-500">
+          <nuxt-link class="font-extrabold hover:opacity-60" to="/projects">
+            Projects
+          </nuxt-link>
         </li>
-        <li>
-          <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
+        <li class="px-2 py-1 text-sm transition-colors duration-500">
+          <nuxt-link class="font-extrabold hover:opacity-60" to="/contact">
+            Contact
+          </nuxt-link>
         </li>
       </ul>
     </div>
   </nav>
 </template>
-
-<style scoped>
-.nav {
-  z-index: 1000;
-  width: 100%;
-  background-color: #fff;
-  color: rgba(55, 65, 81, 1);
-  top: 0;
-  position: sticky;
-  padding: 0.75rem 1rem;
-  margin: 0 auto;
-  border-bottom: 1px solid rgba(229, 231, 235, 1);
-}
-.nav-content {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.nav li {
-  list-style: none;
-}
-.nav-links {
-  display: flex;
-  justify-content: space-between;
-}
-.nav-links li {
-  padding: 0.35rem 0.55rem;
-  transition: background 0.5s;
-  font-size: 0.9rem;
-}
-.nav-link {
-  color: #37352f;
-  font-weight: 800;
-}
-.nav-links li:hover {
-  background: rgba(55, 53, 47, 0.1);
-  border-radius: 5px;
-}
-
-.link-slash {
-  margin-left: 2px;
-  margin-right: 2px;
-  color: rgba(55, 53, 47, 0.4);
-}
-</style>

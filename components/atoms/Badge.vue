62ebf1f37b301922fe41e5c3612720341345ac16
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-01-09 14:32:09 +0000

    Made badge atom

---
+++ components/atoms/Badge.vue
@@ -0,0 +1,20 @@
+<template>
+  <span :class="'px-2 py-1 mr-1 text-xs text-white rounded ' + classes">{{
+    text
+  }}</span>
+</template>
+<script>
+export default {
+  setup() {},
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    classes: {
+      type: String,
+      default: "bg-secondary",
+    },
+  },
+};
+</script>

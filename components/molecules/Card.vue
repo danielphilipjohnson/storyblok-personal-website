b3cfd482a29775489c022bccd88dd5a251aadf83
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2022-02-13 20:28:33 +0000

    Created a card block

---
+++ components/molecules/Card.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="
+      transition
+      duration-300
+      border
+      rounded
+      border-grey
+      hover:shadow-md hover:opacity-90
+    "
+  >
+    <a
+      class="block"
+      :href="link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <figure
+        class="relative flex flex-col justify-between shadow-sm border-gray"
+      >
+  
+        <nuxt-picture
+          v-if="image"
+          class="relative flex flex-col justify-between"
+          provider="storyblok"
+          format="webp"
+          :src="image"
+        />
+      </figure>
+    </a>
+
+    <figcaption class="flex flex-col px-4 pt-3 md:pt-4">
+      <h2
+        class="
+          flex flex-wrap
+          items-center
+          pb-3
+          text-xl
+          font-bold
+          break-all
+          lg:text-3xl
+          md:pb-2
+        "
+      >
+        {{ title }}
+      </h2>
+      <p class="pb-4 text-sm text-gray-600 dark:text-gray-100">
+        {{ description }}
+      </p>
+    </figcaption>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    link: { type: String },
+    image: { type: String },
+    title: { type: String },
+    description: { type: String },
+  },
+};
+</script>
---
+++ components/molecules/HeadingWithText.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{blok}}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>

96fc33fb1870d41399d2ca1cff267b8b95012317
Daniel Philip Johnson <dan@hiyield.co.uk>
2022-06-30 15:42:58 +0100

    fix cannoical url

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
     ]
   },
   publicRuntimeConfig: {
-    BASE_URL: process.env.BASE_URL || 'www.danielphilipjohnson.me',
+    BASE_URL: process.env.BASE_URL || 'https://www.danielphilipjohnson.me',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
